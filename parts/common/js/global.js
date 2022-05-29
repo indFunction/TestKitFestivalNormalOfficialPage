7f675b6f0b23c99a87d243ddbb8a4cc95e163c02
indFunction <indfunction@gmail.com>
2022-05-29 13:04:20 +0900

    壁紙を削除

--- parts/common/js/global.js
+++ parts/common/js/global.js
@@ -1,8 +1,8 @@
 const baseUrl = ''; // '/kitfes'
 const isRoot = location.pathname == baseUrl + '/' || location.pathname == baseUrl + '/index.html' ? true : false;
 
-const backgroundElement = document.getElementById('background');
+// const backgroundElement = document.getElementById('background');
 
-backgroundElement.style.backgroundImage = `url('${isRoot ? '.' : '..'}/media/background/background_fhd.jpg')`;
+// backgroundElement.style.backgroundImage = `url('${isRoot ? '.' : '..'}/media/background/background_fhd.jpg')`;
 
 const getJson = (filePath) => fetch(filePath).then((resp) => resp.json());
