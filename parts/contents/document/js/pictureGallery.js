497523e4c6d1d5fb004a959fddd3b0846fb52b27
indFunction <indfunction@gmail.com>
2022-06-07 00:46:52 +0900

    文章スタイルを更新

--- parts/contents/document/js/pictureGallery.js
+++ parts/contents/document/js/pictureGallery.js
@@ -2,6 +2,8 @@ const pictureGalleryElements = document.getElementsByClassName('pictureGallery')
 let pictureGallerySlideNum = [];
 
 for (let i = 0; i < pictureGalleryElements.length; i++) {
+    if (pictureGalleryElements[i].classList.contains('filled')) continue;
+
     const data = pictureGalleryElements[i].getAttribute('data').split(',');
     const useBorder = pictureGalleryElements[i].getAttribute('useBorder');
     const isSlide = pictureGalleryElements[i].getAttribute('isSlide');
